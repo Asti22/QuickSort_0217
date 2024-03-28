69d9daa6f718b5bfd4639158b2f347de3d2beb1e
Asti22 <uun3461@gmail.com>
2024-03-28 14:04:25 +0700

    melakukan perulangan for pada bagian input,

--- Quicksort.cpp
+++ Quicksort.cpp
@@ -3,3 +3,30 @@ using namespace std;
 
 //Array of integers to hold values
 int arr[20];
+int cmb_count = 0; //number of comparasion
+int mov_count = 0; //number of data movements
+int n;
+
+void input()
+ {
+    while (true)
+    {
+        cout << "Masukan panjang element array: ";
+        cin >> n;
+
+        if ( n <= 20)
+            break;
+        else
+           cout << "\nMaksimum panjang array adalah 20"<<endl;
+
+    }
+    cout <<"\n-------------------" << endl;
+    cout << "\nEnter Array Element" <<endl;
+    cout << "\n-------------------" <<endl;
+
+    for (int i =0; i < n; i++) 
+    {
+        cout << "<" << (i + 1 ) << ">";
+        cin >> arr[i];
+    }
+}
